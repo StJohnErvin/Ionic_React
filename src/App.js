8d78a1651081b9cd43a5d49a3e254f50abd174d6
john <johnjamero007@gmail.com>
2021-08-12 10:28:53 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>My App</h1>
+      </header>
+      <main>
+        <p>Add some content here…</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
