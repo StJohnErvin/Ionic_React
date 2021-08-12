9ae47aa68b7a7b44b2c1ae89bdbf2ea26e34f21d
john <johnjamero007@gmail.com>
2021-08-12 16:55:49 +0800

    basic ionic-react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState} from "react";
+import { IonApp, IonButton, IonContent, IonHeader, IonIcon, IonTitle, IonToast, IonToolbar } from "@ionic/react";
+import { play as playIcon} from 'ionicons/icons';
 
 function App() {
+  const [showToast, setShowToast] = useState(false);
+  const handleClick = () => {
+    setShowToast(true);
+    setTimeout(()=> setShowToast(false), 3000);
+  };
+
   return (
-    <div>
-      <header>
-        <h1>My App</h1>
-      </header>
-      <main>
-        <p>Add some content here…</p>
-      </main>
-    </div>
+    <IonApp>
+      <IonHeader>
+        <IonToolbar color='primary'>  <IonTitle backgroundColor="green">Tri Tag App</IonTitle></IonToolbar>
+
+      </IonHeader>
+            
+      <IonContent className="ion-padding">
+      <h6 style={{ fontSize:'15px'}}>Hello Tri Tag Team!</h6>
+
+        
+        <IonButton color='primary'  onClick={handleClick} style={{height:'2rem', margin:"1px"}} >
+          <IonIcon icon={playIcon} slot='start'/>Start
+        </IonButton>{" "}
+        <IonToast color='primary' isOpen={showToast} message="Start Working!!!"   />
+        
+      </IonContent>
+    </IonApp>
   );
 }
 
